483bc4f503eaff54c95a5ecd0a518d40ae120825
letterwuyu <2558246605@qq.com>
2017-05-07 23:51:27 +0800

    更新def

--- common/def/def.h
+++ common/def/def.h
@@ -1,31 +1,61 @@
-#ifndef _DEF_H_
-#define _DEF_H_
+#ifndef DEF_H
+#define DEF_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <iostream>
-#include <sstream>
-#include <fstream>
-#include <algorithm>
-#include <vector>
-#include <list>
-#include <forward_list>
-#include <deque>
-#include <string>
-#include <map>
-#include <set>
-#include <memory>
-#include <functional>
-#include <chrono>
-#include <futrue>
-#include <thread>
-#include <math>
+//package
+#define DEFINE_MESSAGE_BEGIN(type)           \
+	struct type##Message {                       \
+		type##Message()                          \
+		{                                         \
+			Clear();                              \
+			header_.data_type_ = type;          \
+			header_.data_size_ = sizeof(this) - sizeof(net::NetDataHeader);    \
+		}                                         \
+		net::NetDataHeader  header_ ;               
 
-typedef int64_t      sint64;
-typedef int32_t      sint32;
-typedef uint32_t     uint32;
-typedef uint64_t     uint64;
+#define DEFINE_MESSAGE_END };
 
-#endif /* _TYPEDEFINE_H_ */
+#define PACKAGE_TYPE(type) type##Message
+
+#define CONSTRUCT_MESSAGE(type) type##Message msg
+
+#define UNPACKAGE(type) \
+	SocketEvent*  pSocket = static_cast<SocketEvent*>( static_cast<struct bufferevent*>(pdev) ) \
+	PACKAGE_TYPE(type) *  pPack = static_cast<PACKAGE_TYPE(type) * >(pData)
+
+#define StaticHandle(type) static bool Process##type(void* event, void* data)
+#define ClassHandle(cls, type) cls::Process##type(void* event, void* data)
+
+#define GSingle(type) singleton<type>::intance()
+
+#define GET_TYPE(type) type.header_.data_type_
+#define GET_SIZE(type) type.header_.data_size_
+#define GATE_UNPACK(type, data) PACKAGE_TYPE(type)* pack = static_cast<PACKAGE_TYPE(type) * >(data);
+#define MAX_VOLUME_NAME 20
+
+//log
+#define LOGINFO  	LOGI
+#define LOGWARN  	LOGW
+#define LOGERROR 	LOGE
+#define LOGALARM 	LOGA
+#define LOGFATAL 	LOGF
+#endif //DEF_H
+
+
+//type
+
+#define int32_t  sint32
+#define int64_t  sint64
+#define uint32_t uint32
+#define uint64_t uint64
+
+//handle
+#define Client_Handle(type) ClientProcess_##type(void *pserv, void* pdev, void* pdata)
+#define UN_CLIENT_PACKAGE(type) \
+	SocketEvent*  pSocket = static_cast<SocketEvent*>(static_cast<struct bufferevent*>(psrv) ) \
+    SocketEvent*  pSocket = static_cast<SocketEvent*>( static_cast<struct bufferevent*>(pdev) ) \
+    PACKAGE_TYPE(type) *  pPack = static_cast<PACKAGE_TYPE(type) * >(pData)
+
+
+//
+
+#define MAX_COPY_NUM 2
